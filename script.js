519e0f9ffa4345a11a051e395f2911eb9e009f94
Raza Sultan <120782387+lucifer8624@users.noreply.github.com>
2025-10-28 15:33:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,47 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+  constructor(width, height) {
+    if (width <= 0 || height <= 0) {
+      throw new Error("Width and height must be positive numbers");
+    }
+    this._width = width;
+    this._height = height;
+  }
 
-class Square extends Animal {}
+  // Getter for width
+  get width() {
+    return this._width;
+  }
+
+  // Getter for height
+  get height() {
+    return this._height;
+  }
+
+  // Method to calculate area
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(side) {
+    if (side <= 0) {
+      throw new Error("Side length must be a positive number");
+    }
+    super(side, side); // Call parent constructor with equal width and height
+  }
+
+  // Method to calculate perimeter
+  getPerimeter() {
+    return 4 * this.width;
+  }
+}
+
+// Make classes accessible globally (important for Cypress tests)
+window.Rectangle = Rectangle;
+window.Square = Square;
+
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
